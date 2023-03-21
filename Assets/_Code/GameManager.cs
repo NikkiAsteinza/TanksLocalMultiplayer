92ada094547b22f3442413e26e1dbe40816549d9
Nikki A.A <nikki.asteinza@gmail.com>
2023-03-21 02:15:36 +0100

    Game manager script and prefab

---
+++ Assets/_Code/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Tanks.Tanks
+{
+    public class GameManager : SingletonBehaviour<GameManager>
+    {
+        [SerializeField] float loadLerpDuration = 0.4f;
+        [SerializeField] GameObject loadCanvas;
+        [SerializeField] CanvasGroup canvasGroup;
+        [SerializeField] SceneReference mainMenu;
+        [SerializeField] SceneReference gameplayScene;
+
+        public AnimationCurve animationCurve;
+        int selectedPlayers = 1;
+
+        private void Awake()
+        {
+            loadCanvas.SetActive(false);
+            canvasGroup.alpha = 0;
+            animationCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+        }
+
+        public void InitGame()
+        {
+            StartCoroutine(LoadSceneCoroutine(gameplayScene));
+        }
+        public void GoToMainMenu()
+        {
+            StartCoroutine(LoadSceneCoroutine(mainMenu));
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        private IEnumerator LoadSceneCoroutine(SceneReference sceneToLoad)
+        {
+            loadCanvas.SetActive(true);
+            StartCoroutine(LerpCanvas(0, 1));
+            yield return new WaitUntil(() => canvasGroup.alpha == 1);
+            AsyncOperation load = SceneManager.LoadSceneAsync(sceneToLoad.ScenePath);
+            yield return new WaitUntil(() => load.isDone == true);
+            StartCoroutine(LerpCanvas(1, 0));
+            yield return new WaitUntil(() => canvasGroup.alpha == 0);
+            loadCanvas.SetActive(false);
+        }
+        private IEnumerator LerpCanvas(int from, int to)
+        {
+            var startTime = Time.time;
+            var endTime = Time.time + loadLerpDuration;
+            var elapsedTime = 0f;
+
+            canvasGroup.alpha = animationCurve.Evaluate(from);
+            while (Time.time <= endTime)
+            {
+                elapsedTime = Time.time - startTime; // update the elapsed time
+                var percentage = 1 / (loadLerpDuration / elapsedTime); // calculate how far along the timeline we are
+
+                canvasGroup.alpha = animationCurve.Evaluate(percentage);
+                yield return new WaitForEndOfFrame(); // wait for the next frame before continuing the loop
+            }
+            canvasGroup.alpha = animationCurve.Evaluate(to);
+        }
+    }
+}
