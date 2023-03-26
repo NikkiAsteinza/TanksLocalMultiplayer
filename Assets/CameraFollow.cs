7250e9cc039aeca09343212e924414583a7a5c38
Nikki A.A <nikki.asteinza@gmail.com>
2023-03-26 14:59:26 +0200

    Camera follow script

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,60 @@
+
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Vector3 offsetPosition;
+
+    [SerializeField]
+    private Space offsetPositionSpace = Space.Self;
+
+    [SerializeField]
+    private bool lookAt = true;
+
+    private void Update()
+    {
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+
+            return;
+        }
+
+        SetTargetPosition();
+
+        SetTargetRotation();
+    }
+
+    private void SetTargetRotation()
+    {
+        if (lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
+    }
+
+    private void SetTargetPosition()
+    {
+        if (offsetPositionSpace == Space.Self)
+        {
+            transform.position = target.TransformPoint(offsetPosition);
+        }
+        else
+        {
+            transform.position = target.position + offsetPosition;
+        }
+    }
+}
